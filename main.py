0248c84f666d7136c26006ab98ef2b5ebee7eb16
EllaMarketingProyectoAI <proyectoai@ella.marketing>
2025-07-31 10:24:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -83,4 +83,5 @@ def dividir_podcast():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
